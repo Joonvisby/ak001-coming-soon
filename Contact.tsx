97839cf6cb5dba394a7f7165bbddb79a3820003e
joony <joony@mac.lan>
2025-08-08 18:56:56 +0900

    Fix Contact component: remove unused Tally import

--- Contact.tsx
+++ Contact.tsx
@@ -2,7 +2,6 @@
 
 import { motion } from 'framer-motion'
 import { useState } from 'react'
-import { submitToTally } from './lib/tally'
 
 export default function Contact() {
   const [email, setEmail] = useState('')
@@ -80,8 +79,6 @@ export default function Contact() {
               {isSubmitting ? 'Subscribing...' : isSubmitted ? 'Subscribed!' : 'Subscribe'}
             </button>
           </form>
-          
-
           
           {isSubmitted && (
             <motion.p
